b121334c2c38e399b2c8808b38a43832bc8e49b4
speed-pretil <speed@pretil.com>
2023-09-07 11:05:27 +0200

    Remove build, install, and log files

--- build/somorob/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/speed/Somorob_Dynamixel/prefab.MobileTrunk/test/ros/src/somorob
-# Build directory: /home/speed/Somorob_Dynamixel/build/somorob
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
